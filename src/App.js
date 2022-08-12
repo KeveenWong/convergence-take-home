fd7a74ff124a5103e9194e4aecf1e94fcef66c2e
KeveenWong <67565283+KeveenWong@users.noreply.github.com>
2022-08-11 21:56:21 -0700

    Add scrolling, chevron buttons, and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 function App() {
   const images_url = 'https://jsonplaceholder.typicode.com/photos'
@@ -11,12 +12,25 @@ function App() {
       fetch(images_url)
       .then((response) => response.json())
       .then((data) => {
-      setImages(data.map((image) => <img src={image.thumbnailUrl} alt='/' />));
+      setImages(data.map((image) => <img className='w-[370px] inline-block cursor-pointer hover:scale-105 ease-in-out duration-300'
+                                          src={image.thumbnailUrl} 
+                                          alt='a photo :D' />));
       });
     } catch {
       console.log("error getting and setting images.");
     }
   }
+
+  const slideLeft = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft - 1200;
+  }
+
+  const slideRight = () => {
+    var slider = document.getElementById('slider');
+    slider.scrollLeft = slider.scrollLeft + 1200;
+  }
+
 
   // calls API call to update state *after* all components rendered
   React.useEffect(() => {
@@ -25,10 +39,14 @@ function App() {
   
   return (
     <>
-    <h1>Convergence.</h1>
-    <div>
-      {images}
-    </div>
+      <h1>Convergence.</h1>
+      <div className='relative flex items-center'>
+        <MdChevronLeft className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideLeft} size={40} />
+        <div id='slider' className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth hover:scroll-auto scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-300'>
+          {images}
+        </div>
+        <MdChevronRight className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideRight} size={40} />
+      </div>
     </>
   )
 }
